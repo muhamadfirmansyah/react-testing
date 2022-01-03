f0b57a4d1b703f3551cea106c4469b1431cc6b2e
Fnwrkio <firmansyah.assignment@gmail.com>
2022-01-03 14:17:54 +0700

    feat: Snapshot Testing

---
+++ src/components/Link.component.test.js
@@ -0,0 +1,30 @@
+import renderer from 'react-test-renderer'
+import Link from './Link.component'
+
+test('Link changes the class when hovered', () => {
+
+	const { act } = renderer
+
+	const component = renderer.create(
+		<Link page="https://www.facebook.com">Facebook</Link>)
+
+	let tree = component.toJSON()
+	expect(tree).toMatchSnapshot()
+
+	act(() => {
+
+		// manually trigger the callback
+		tree.props.onMouseEnter()
+		// re-rendering
+		tree = component.toJSON()
+		expect(tree).toMatchSnapshot()
+	})
+
+	act(() => {
+		// manually trigger the callback
+		tree.props.onMouseLeave()
+		// re-rendering
+		tree = component.toJSON()
+		expect(tree).toMatchSnapshot()
+	})
+})
